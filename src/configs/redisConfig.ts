f86e746172579050acf2c462c0d0311ffab68523
aditya bhojane <bhojaneaditya7@gmail.com>
2025-01-07 05:46:03 +0530

    add redis connection

--- src/configs/redisConfig.ts
+++ src/configs/redisConfig.ts
@@ -1,6 +1,18 @@
 import { REDIS_PORT, REDIS_URL } from "./severConfig";
+import Redis from "ioredis";
 
 export const redisConfig = {
     host:REDIS_URL,
     port:REDIS_PORT
 }
+
+
+const redis = new Redis(REDIS_URL);
+
+redis.on("connect", () => {
+  console.log("Connected to Redis!");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
